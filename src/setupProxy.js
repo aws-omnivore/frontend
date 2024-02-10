6aa73c2600a3448f224243e8c4b1e8d45e6d1fbb
PANG-J <rhkdgus1430@gmail.com>
2024-02-10 23:10:35 +0900

    feat: API호출을 위한 proxy기능 추가

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+// src/setupProxy.js
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://api.sketch-food.com:443',
+      changeOrigin: true,
+    })
+  );
+};
